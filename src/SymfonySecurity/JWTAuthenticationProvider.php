34c94e03a5fbf3ed8cd6dcf58df976463d758fd2
DerManoMann <mano@radebatz.net>
2017-11-02 16:45:35 +1300

    Add missing use statement

--- src/SymfonySecurity/JWTAuthenticationProvider.php
+++ src/SymfonySecurity/JWTAuthenticationProvider.php
@@ -7,6 +7,7 @@ use Evaneos\JWT\Util\JWTUserBuilder;
 use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class JWTAuthenticationProvider implements AuthenticationProviderInterface
 {
